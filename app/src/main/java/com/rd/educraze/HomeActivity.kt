dd2424b79fd060cc3582d39134cbf4cacb5dbadf
Rd4dev <rd4ramadevi@gmail.com>
2023-02-06 22:26:43 +0530

    Empty Home Activity Added

---
+++ app/src/main/java/com/rd/educraze/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.rd.educraze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
